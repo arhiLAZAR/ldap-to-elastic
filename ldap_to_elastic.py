1e95943f851e73dbb7cb3f4cfef9c3962df842fc
Anton Evdokimov <evdokimov@selectel.ru>
2021-08-31 13:46:39 +0300

    Add base logic

---
+++ ldap_to_elastic.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python
+
+import ldap,requests,json,os
+
+DEBUG = "1"
+
+ldapDomain = os.environ['L2E_LDAP_DOMAIN']
+ldapBindDN = os.environ['L2E_LDAP_LOGIN']
+ldapPassword = os.environ['L2E_LDAP_PASS']
+ldapBaseDN = os.environ['L2E_LDAP_BASE_DN']
+ldapFilter = os.environ['L2E_LDAP_FILTER']
+
+elasticLogin = os.environ['L2E_ELASTIC_LOGIN']
+elasticPassword = os.environ['L2E_ELASTIC_PASS']
+
+def getLdapUsers():
+  CACERTFILE="ca.crt"
+
+  ldapURL = "ldaps://" + ldapDomain + ":636"
+
+  l = ldap.initialize(ldapURL)
+
+  l.set_option(ldap.OPT_X_TLS_CACERTFILE,CACERTFILE)
+
+  l.simple_bind_s(ldapBindDN,ldapPassword)
+  ldapResponse = l.search_s(ldapBaseDN, ldap.SCOPE_SUBTREE, ldapFilter)
+
+  ldapUsers = []
+  for u in ldapResponse:
+    ldapUsers.append(u[1]["cn"][0].decode("utf-8"))
+
+  ldapUsers.sort()
+
+  if DEBUG:
+    print("Found following users in LDAP:")
+    for ldapUser in ldapUsers:
+      print(ldapUser)
+
+  return ldapUsers
+
+
+def getElasticUsers():
+  url = 'https://localhost:9200/_security/user'
+  headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
+
+  from urllib3.exceptions import InsecureRequestWarning
+  requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
+
+  elasticUsers = []
+  elasticResponse = requests.get(url, headers=headers, auth=requests.auth.HTTPBasicAuth(elasticLogin, elasticPassword), verify=False)
+  for user, params in elasticResponse.json().items():
+    elasticUsers.append(params['username'])
+
+  elasticUsers.sort()
+
+  if DEBUG:
+    print("\nFound following users in elasticsearch:")
+    for elasticUser in elasticUsers:
+      print(elasticUser)
+
+
+  return elasticUsers
+
+
+def createElasticUser(username):
+  print("Create new elasticsearch user " + username, end='\t\t')
+
+  url = 'https://localhost:9200/_security/user/' + username
+  payload= {"password": "123123", "roles": ["superuser", "kibana_admin"]}
+  headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
+
+
+  from urllib3.exceptions import InsecureRequestWarning
+  requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
+
+  elasticResponse = requests.post(url, json=payload, headers=headers, auth=requests.auth.HTTPBasicAuth(elasticLogin, elasticPassword), verify=False)
+
+  print("Done!")
+
+
+def main():
+  ldapUsers = getLdapUsers()
+  elasticUsers = getElasticUsers()
+
+  for ldapUser in ldapUsers:
+    if ldapUser not in elasticUsers:
+      createElasticUser(ldapUser)
+
+main()
